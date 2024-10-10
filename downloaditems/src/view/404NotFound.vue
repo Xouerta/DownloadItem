33722e840fa15827a0cdf7ecbb53ee3be5b1294b
Xouerta <2236613237@qq.com>
2024-10-10 20:42:14 +0800

    day0 commit

---
+++ downloaditems/src/view/404NotFound.vue
@@ -0,0 +1,35 @@
+
+
+<template>
+<h1>404 Not Found</h1>
+  <h2>抱歉，页面走丢了！</h2>
+  <a href="javascript:"@click="goHome">点我回家！或者等待<span>{{ countDown }}</span>s后自动跳转</a>
+</template>
+
+<script>
+export default {
+  name: '404NotFound',
+  data(){
+    return{
+      countDown:4,
+      timer: null
+    }
+  },
+  methods:{
+    goHome(){
+      this.$router.replace({path:'/'})
+      clearInterval(this.timer)
+    }
+  },
+  created() {
+    this.timer = setInterval(() => {
+      this.countDown >0 ? this.countDown-- : this.goHome()
+    },1000)
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
